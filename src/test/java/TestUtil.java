031dcdf8c8ce24a99801efb708322b0d61212907
ChiefTwoPencils <robertwilk916@gmail.com>
2016-03-24 19:53:09 -0700

    Kicked its ass....

--- src/test/java/TestUtil.java
+++ src/test/java/TestUtil.java
@@ -1,18 +1,98 @@
-import atm.ATM;
+import atm.transaction.Withdrawal;
+import banking.Card;
 import simulation.Simulation;
 
-import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.lang.reflect.Field;
-import java.net.InetAddress;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.net.UnknownHostException;
+import java.util.*;
 
 /**
  * Created by Robert Wilk
  * on 3/24/2016.
  */
 public class TestUtil {
+
+    public static final int SHORT_SLEEP = 1000;
+    public static final int MEDIUM_SLEEP = 2500;
+    public static final int LONG_SLEEP = 5000;
+
+    public enum Transaction {
+        WITHDRAWAL(KeyEvent.VK_1),
+        DEPOSIT(KeyEvent.VK_2),
+        TRANSFER(KeyEvent.VK_3),
+        BAL_INQUIRY(KeyEvent.VK_4);
+
+        private int keyEvent;
+
+        Transaction(int option) {
+            this.keyEvent = option;
+        }
+
+        public int getKeyEvent() {
+            return keyEvent;
+        }
+    }
+
+    public enum Account {
+        CHECKING(KeyEvent.VK_1),
+        SAVINGS(KeyEvent.VK_2),
+        MONEY_MARKET(KeyEvent.VK_3);
+
+        private int keyEvent;
+
+        Account(int option) {
+            this.keyEvent = option;
+        }
+
+        public int getKeyEvent() {
+            return keyEvent;
+        }
+    }
+
+    public enum WithdrawalAmount {
+        TWENTY(1, KeyEvent.VK_1),
+        FORTY(2, KeyEvent.VK_2),
+        SIXTY(3, KeyEvent.VK_3),
+        ONE_HUNDRED(5, KeyEvent.VK_4),
+        TWO_HUNDRED(10, KeyEvent.VK_5);
+
+        private static final int base = 20;
+
+        private int amount;
+        private int keyEvent;
+
+        WithdrawalAmount(int factor, int keyEvent) {
+            amount = base * factor;
+            this.keyEvent = keyEvent;
+        }
+
+        public int getAmount() {
+            return amount;
+        }
+
+        public int getKeyEvent() {
+            return keyEvent;
+        }
+    }
+
+    public enum Choice {
+        YES(KeyEvent.VK_1), NO(KeyEvent.VK_2);
+
+        private int keyEvent;
+
+        Choice(int keyEvent) {
+            this.keyEvent = keyEvent;
+        }
+
+        public int getKeyEvent() {
+            return keyEvent;
+        }
+    }
 
     /**
      * Starts a new instance of the ATM simulator.
@@ -54,7 +134,7 @@ public class TestUtil {
         Object panel = getObjectByField(gui, "billsPanel");
         TextField billsNumberField = (TextField) getObjectByField(panel, "billsNumberField");
         billsNumberField.setText(String.valueOf(cash));
-        new Robot().keyPress(KeyEvent.VK_ENTER);
+        pressEnter();
     }
 
     /**
@@ -64,11 +144,84 @@ public class TestUtil {
      * @throws NoSuchFieldException
      * @throws IllegalAccessException
      */
-    public Label[] getCurrentDisplay(Simulation simulation)
+    public static Label[] getCurrentDisplay(Simulation simulation)
     throws NoSuchFieldException, IllegalAccessException {
         Object simDisplay = getObjectByField(simulation, "display");
-
         return (Label[]) getObjectByField(simDisplay, "displayLine");
+    }
+
+    /**
+     * Inserts a card with the given card number.
+     * @param simulation The current simulation.
+     * @param cardNumber The user's card number.
+     * @throws NoSuchFieldException
+     * @throws IllegalAccessException
+     * @throws InterruptedException
+     * @throws AWTException
+     */
+    public static void insertCard(Simulation simulation, int cardNumber)
+            throws NoSuchFieldException, IllegalAccessException, InterruptedException, AWTException {
+        Button simReader = (Button) getObjectByField(simulation, "cardReader");
+        Arrays.asList(simReader.getActionListeners()).forEach(l -> l.actionPerformed(null));
+        Object panel = getObjectByField(simulation.getGUI(), "cardPanel");
+        TextField cardNumberField = (TextField) getObjectByField(panel, "cardNumberField");
+        Thread.sleep(MEDIUM_SLEEP);
+        cardNumberField.setText(String.valueOf(1));
+        pressEnter();
+    }
+
+    /**
+     * Enters the password after a card was inserted.
+     * @param simulation The current simulation
+     * @param password The password for the inserted card.
+     * @throws NoSuchFieldException
+     * @throws IllegalAccessException
+     * @throws AWTException
+     */
+    public static void enterPassword(Simulation simulation, String password)
+            throws NoSuchFieldException, IllegalAccessException, AWTException {
+        Object keyboard = getObjectByField(simulation, "keyboard");
+        ((StringBuffer) getObjectByField(keyboard, "currentInput")).append(password);
+        pressEnter();
+    }
+
+    /**
+     * Chooses a transaction type if prompted by simulating the appropriate key press.
+     * @param transaction The transaction type.
+     * @throws NoSuchFieldException
+     * @throws IllegalAccessException
+     * @throws AWTException
+     */
+    public static void chooseTransactionType(TestUtil.Transaction transaction)
+            throws NoSuchFieldException, IllegalAccessException, AWTException {
+        press(transaction.getKeyEvent());
+    }
+
+    /**
+     * Chooses an account type if prompted by simulating the appropriate key press.
+     * @param account The account type.
+     * @throws AWTException
+     */
+    public static void chooseAccountType(TestUtil.Account account) throws AWTException {
+        press(account.getKeyEvent());
+    }
+
+    /**
+     * Chooses a withdrawal amount if prompted by simulating the appropriate key press.
+     * @param withdrawalAmount The amount to withdrawal.
+     * @throws AWTException
+     */
+    public static void chooseWithdrawalType(TestUtil.WithdrawalAmount withdrawalAmount) throws AWTException {
+        press(withdrawalAmount.getKeyEvent());
+    }
+
+    /**
+     * Makes a yes/no choice if prompted by simulating the appropriate key press.
+     * @param choice The choice made.
+     * @throws AWTException
+     */
+    public static void chooseYesOrNo(Choice choice) throws AWTException {
+        press(choice.getKeyEvent());
     }
 
     /**
@@ -108,6 +261,14 @@ public class TestUtil {
         return field.get(object);
     }
 
+    static void pressEnter() throws AWTException {
+        press(KeyEvent.VK_ENTER);
+    }
+
+    static void press(int keyEvent) throws AWTException {
+        new Robot().keyPress(keyEvent);
+    }
+
     /**
      * A test of the TestUtil functionality.
      * @param args Not used.
@@ -121,16 +282,24 @@ public class TestUtil {
             throws UnknownHostException,
             InterruptedException,
             NoSuchFieldException,
-            IllegalAccessException, AWTException {
+            IllegalAccessException, AWTException, NoSuchMethodException, InvocationTargetException {
         ATMMain atmMain = new ATMMain();
         Simulation simulation = atmMain.getTheSimulation();
-        Thread.sleep(100);
+        Thread.sleep(SHORT_SLEEP);
         TestUtil.turnAtmOn(simulation);
-        Thread.sleep(100);
+        Thread.sleep(SHORT_SLEEP);
         TestUtil.setInitialCash(simulation, 200);
-        Label[] display = new TestUtil().getCurrentDisplay(simulation);
-        Thread.sleep(100);
-        for (Label label : new TestUtil().getCurrentDisplay(simulation))
-            System.out.println(label.getText());
+        Thread.sleep(SHORT_SLEEP);
+        TestUtil.insertCard(simulation, 1);
+        Thread.sleep(SHORT_SLEEP);
+        TestUtil.enterPassword(simulation, "42");
+        Thread.sleep(SHORT_SLEEP);
+        TestUtil.chooseTransactionType(Transaction.WITHDRAWAL);
+        Thread.sleep(SHORT_SLEEP);
+        TestUtil.chooseAccountType(Account.CHECKING);
+        Thread.sleep(SHORT_SLEEP);
+        TestUtil.chooseWithdrawalType(WithdrawalAmount.FORTY);
+        Thread.sleep(LONG_SLEEP * 4);
+        TestUtil.chooseYesOrNo(Choice.YES);
     }
 }
